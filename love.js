900126e20b4fffaeefb1f1775765b9119ea825e9
unknown <sharminmily039@gmail.comm>
2022-08-15 16:35:57 +0600

    2nd commit

--- love.js
+++ love.js
@@ -1,13 +1,13 @@
 document.getElementById("yes").addEventListener("click", function () {
-  document.getElementById("msg").innerText = "I LOVE YOU 💘 ";
+  document.getElementById("msg").innerText = "I LOVE 💘 ";
   commnFun2("msg");
 });
 // 
 function loveLetter() {
   commnFun2("msg");
-    document.getElementById("msg").innerText = "I LOVE YOU JOHON💝 ";
+    document.getElementById("msg").innerText = "I LOVE YOU RANUMONDAL...💝 ";
 }
 
 function commnFun2(id) {
-  document.getElementById(id).innerText = "I LOVE YOU JOHON💘 ";
+  document.getElementById(id).innerText = "I LOVE YOU HEROALOM...💘 ";
 }
